f07d180bf5bce36a0e710e7090ca812e7339e52b
Joe DeVivo <joe@devivo.com>
2017-12-17 13:22:56 -0700

    Rearanged ergodox thumbs

--- keyboards/ergodox_ez/keymaps/joedevivo2/keymap.c
+++ keyboards/ergodox_ez/keymaps/joedevivo2/keymap.c
@@ -43,47 +43,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |      |  `~  |  Meh | Left | Right|                                       |  Up  | Down |   [  |   ]  | Raise|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |⌘/Esc| Alt   |       | Ctrl | ⌘    |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 |Backsp| Del  |------|       |------|  Enter |Space |
- *                                 |ace   |      | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                        |⌘/Esc| Alt  |       | Ctrl | ⌘    |
+ *                                 ,------|------|------|       |------+-------+------.
+ *                                 |      |      | Home |       | PgUp |       |      |
+ *                                 |Backsp| Del  |------|       |------| Enter |Space |
+ *                                 |ace   |      | End  |       | PgDn |       |      |
+ *                                 `--------------------'       `---------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [_QWERTY] = KEYMAP(  // layer 0 : default
     // left hand
-    // Lightcycle Test - 
-    //KC_EQL, KC_1,        KC_2,    KC_3,    KC_4,    KC_5,   XXXXXX,
-    XXXXXXX,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    // Lightcycle Test -
+    KC_EQL, KC_1,        KC_2,    KC_3,    KC_4,    KC_5,   XXXXXXX,
+    //XXXXXXX,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     KC_TAB,   KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,
     CTL_ESC,  KC_A,        KC_S,    KC_D,    KC_F,    KC_G,
-    KC_LSFT,  ALT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,
-    ADJUST,   KC_GRV,      KC_MEH,  KC_LEFT, KC_RGHT,
-                                                       KC_LGUI, KC_LALT,
+    KC_LSFT,  ALT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_MEH,
+    ADJUST,   KC_MEH,      KC_LALT, KC_LGUI, LOWER,
+                                                       KC_LCTL, KC_LALT,
                                                                 KC_HOME,
-                                LT(_LOWER, KC_BSPACE), KC_DELETE, LOWER,
+                                          KC_BSPACE, KC_DELETE, KC_LGUI,
     // right hand
     // Lightcycle Test -
-    //  XXXXXXX,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,           KC_MINS,
-        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX,
-        XXXXXXX,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,           KC_BSLS,
-                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,        KC_QUOT,
-        XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  ALT_T(KC_SLSH), KC_RSFT,
-                            KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,        XXXXXXX,
+        XXXXXXX,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
+    //    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+        XXXXXXX,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
+                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+        XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                           RAISE,   KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,
         KC_LCTL, KC_LGUI,
-        KC_PGUP,
-        RAISE, KC_ENTER, LT(_RAISE, KC_SPACE)
+        KC_END,
+        KC_LGUI, KC_ENTER, KC_SPACE
     ),
 
   [_LOWER] = KEYMAP(
-    // KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, 
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, 
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, 
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, 
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, 
-    _______, _______, _______, _______, _______, 
+    // KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,
+    _______, _______, _______, _______, _______,
                                                   _______, _______,
                                                            _______,
                                          _______, _______, _______,
@@ -100,16 +100,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_RAISE] = KEYMAP(
-    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,   
+    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,
-    _______,  XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, 
+    _______,  XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX,
     _______,  XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, _______,
-    _______,  _______, _______, _______, _______, 
+    _______,  _______, _______, _______, _______,
                                                   _______, _______,
                                                            _______,
                                          _______, _______, _______,
 
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
              KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______,
@@ -120,16 +120,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_ADJUST] = KEYMAP(
-    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,   
+    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     _______,  RESET,   _______, _______, _______, _______, _______,
-    _______,  EPRM,    _______, AU_ON,   AU_OFF,  AG_NORM, 
+    _______,  EPRM,    _______, AU_ON,   AU_OFF,  AG_NORM,
     _______,  _______, VRSN,    _______, _______, _______, _______,
-    _______,  _______, RGB_MOD, RGB_VAD, RGB_VAI, 
+    _______,  _______, RGB_MOD, RGB_VAD, RGB_VAI,
                                                   _______, _______,
                                                            _______,
                                          _______, _______, _______,
 
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, _______, _______, _______, _______, _______, _______,
              AG_SWAP, QWERTY,  _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,
