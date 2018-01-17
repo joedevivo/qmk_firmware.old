22e3b3b6b3dc5cb4f8e3c6a25c43b31d10d6c627
Joe DeVivo <joe@devivo.com>
2018-01-17 04:10:51 -0700

    switch kinesis default to stapelberg

--- keyboards/kinesis/rules.mk
+++ keyboards/kinesis/rules.mk
@@ -1,7 +1,7 @@
 
 ## Project specific files
 
-SRC=  matrix.c 
+SRC=  matrix.c
 
 # MCU name
 MCU = at90usb1286
@@ -72,4 +72,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no # Audio output should be port E6, current quantum library hardcodes C6, which we use for programming
 CUSTOM_MATRIX=yes # need to do our own thing with the matrix
 
-DEFAULT_FOLDER = kinesis/alvicstep
+DEFAULT_FOLDER = kinesis/stapelberg
