6cd7e4c70ca4a8cda38d67e9e57a29d4958f1b05
Joe DeVivo <joe@devivo.com>
2018-04-30 07:29:15 -0700

    Mouse for Levinson

--- keyboards/atreus62/keymaps/joedevivo/keymap.c
+++ keyboards/atreus62/keymaps/joedevivo/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS },
     { KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS },
     { CTL_ESC,  KC_A,    KC_S,    MOUSED,  KC_F,    KC_G,    XXXXXXX,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
-    { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LTHUMBO,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
+    { KC_LSFT,  ALT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B,    LTHUMBO,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
     { ADJUST,   KC_MEH,  KC_LALT, KC_LGUI, LTHUMBI, KC_BSPC, RTHUMBO,  KC_SPC,  RTHUMBI, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT }
   },
 
