71e0ae84fad8f2124d32f453faca4973f6c12c66
Joe DeVivo <joe@devivo.com>
2018-09-10 14:15:25 -0700

    dactyl manuform

--- keyboards/ergodox_ez/keymaps/joedevivo/keymap.c
+++ keyboards/ergodox_ez/keymaps/joedevivo/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                             MO(_FN),
                                         KC_BSPACE, KC_DELETE, LOWER,
     // right hand
-    SPCMCS,  KC_6, KC_7,   KC_8,   KC_9,    KC_0,           KC_MINS,
+    SPCMAC,  KC_6, KC_7,   KC_8,   KC_9,    KC_0,           KC_MINS,
     KC_ENTER,KC_Y, KC_U,   KC_I,   KC_O,    KC_P,           KC_BSLS,
              KC_H, KC_J,   KC_K,   KC_L,    KC_SCLN,        KC_QUOT,
     KC_MEH,  KC_N, KC_M,   KC_COMM,KC_DOT,  ALT_T(KC_SLSH), OSM_LSFT,
