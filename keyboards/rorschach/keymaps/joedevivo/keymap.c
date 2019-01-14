4d66fa355db0a9f80581b1e84336a1e6ddb92267
Joe DeVivo <joe@devivo.com>
2019-01-14 07:31:48 -0700

    moved arrows

--- keyboards/rorschach/keymaps/joedevivo/keymap.c
+++ keyboards/rorschach/keymaps/joedevivo/keymap.c
@@ -22,8 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   CTL_ESC, KC_A,    KC_S,    MOUSED,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, ALT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LEFT, KC_RIGHT,KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_SPC,  RAISE,   KC_MEH,  SPCMAC,  KC_DOWN, KC_UP,   \
-                                      KC_DEL,  KC_LSFT, KC_RSFT, KC_ENT \
+  XXXXXXX, KC_LEFT, KC_RIGHT,KC_LALT, KC_LGUI, LOWER,   RAISE,   KC_MEH,  SPCMAC,  KC_DOWN, KC_UP,   XXXXXXX, \
+                                      KC_BSPC, KC_DEL,  KC_ENT,  KC_SPC \
 ),
 
 /* Lower
