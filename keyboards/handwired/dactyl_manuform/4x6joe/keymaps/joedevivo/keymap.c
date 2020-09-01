3eef31573c357837a99c2d48394925968c3ac614
Joe DeVivo <joe@devivo.com>
2020-09-01 06:25:38 -0700

    keymap updates

--- keyboards/handwired/dactyl_manuform/4x6joe/keymaps/joedevivo/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6joe/keymaps/joedevivo/keymap.c
@@ -7,9 +7,9 @@ extern uint8_t is_master;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_4x6( \
     KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,  KC_T,                                      KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    CTL_ESC, KC_A, KC_S,    MOUSED,  KC_F,  KC_G,                                      KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    CTL_ESC, KC_A, KC_S,    MOUSED,  ARROWF,KC_G,                                      KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,  KC_B,                                      KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                   KC_LEFT, KC_RIGHT,LOWER, KC_BSPC, KC_DEL, KC_LGUI, KC_RGUI, KC_ENT, KC_SPC, RAISE, KC_DOWN, KC_UP, \
+                   KC_LEFT, KC_RIGHT,LOWER, KC_BSPC, KC_DEL, KC_LGUI, KC_MEH, KC_ENT, KC_SPC, RAISE, KC_DOWN, KC_UP, \
                                                      KC_MEH, KC_LALT, KC_RALT, KC_RCTL \
    ),
   [_LOWER] = LAYOUT_4x6( \
--- keyboards/idobo/keymaps/joedevivo/keymap.c
+++ keyboards/idobo/keymaps/joedevivo/keymap.c
@@ -7,11 +7,11 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_5x15(
-  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _,_,_,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _,_,_,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  CTL_ESC, KC_A,    KC_S,    MOUSED,  ARROWF,  KC_G,    _,_,_,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _,_,_,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LEFT, KC_RGHT, KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_DEL,_,KC_ENT, KC_SPC,  RAISE,   KC_MEH,  SPCMAC,  KC_DOWN, KC_UP
+  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _,     _,     _,      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7, KC_P8 ,KC_P9,  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+  CTL_ESC, KC_A,    KC_S,    MOUSED,  ARROWF,  KC_G,    KC_P4, KC_P5, KC_P6,  KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1, KC_P2, KC_P3,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  _,       KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_DEL,KC_MEH,KC_ENT, KC_SPC, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_UP
 ),
 
 [_LOWER] = LAYOUT_ortho_5x15(
